0a7fe3ec8e85764fb48cd2767fda0a7b7dd37dbf
ACF <alricfernandes11@gmail.com>
2024-06-05 11:45:11 -0400

    4. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,11 +7,14 @@ contract Token {
   string public name;
   string public symbol;
   uint256 public decimals = 18;
-  uint256 public totalSupply; // 1,000,000 x 10^18
+  uint256 public totalSupply;
+
+  mapping(address => uint256) public balanceOf;
 
   constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
     name = _name;
     symbol = _symbol;
     totalSupply = _totalSupply * (10**decimals);
+    balanceOf[msg.sender] = totalSupply;
   }
 }
